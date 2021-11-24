80a565452c321ef676050d52ff8dbef53d04f014
Jon Durrant <jon@jondurrant.com>
2021-11-24 12:45:54 +0000

    Working prior to refactor

--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,8 @@
 #include "MQTTRouterPing.h"
 #include "MQTTRouterTwin.h"
 
+#include <WifiHelper.h>
+
 #include "StateExample.h"
 #include "ExampleAgentObserver.h"
 
@@ -85,6 +87,121 @@ void startMQTT(){
 
 lwespr_t lwesp_callback_func(struct lwesp_evt *evt){
 	printf("Init Callback\n");
+	switch(evt->type){
+	case LWESP_EVT_INIT_FINISH:{
+		LogDebug(("Callback LWESP_EVT_INIT_FINISH"));
+		break;
+	}
+	case LWESP_EVT_RESET_DETECTED:{
+		LogDebug(("Callback LWESP_EVT_INIT_FINISH"));
+		break;
+	}
+	case LWESP_EVT_RESET:{
+		LogDebug(("Callback LWESP_EVT_INIT_FINISH"));
+		break;
+	}
+	case LWESP_EVT_RESTORE:{
+		LogDebug(("Callback LWESP_EVT_INIT_FINISH"));
+		break;
+	}
+	case LWESP_EVT_CMD_TIMEOUT:{
+		LogDebug(("Callback LWESP_EVT_INIT_FINISH"));
+		break;
+	}
+	case LWESP_EVT_DEVICE_PRESENT:{
+		LogDebug(("Callback LWESP_EVT_INIT_FINISH"));
+		break;
+	}
+	case LWESP_EVT_AT_VERSION_NOT_SUPPORTED:{
+		LogDebug(("Callback LWESP_EVT_INIT_FINISH"));
+		break;
+	}
+	case LWESP_EVT_CONN_RECV:{
+		LogDebug(("Callback LWESP_EVT_INIT_FINISH"));
+		break;
+	}
+	case LWESP_EVT_CONN_SEND:{
+		LogDebug(("Callback LWESP_EVT_INIT_FINISH"));
+		break;
+	}
+	case LWESP_EVT_CONN_ACTIVE:{
+		LogDebug(("Callback LWESP_EVT_INIT_FINISH"));
+		break;
+	}
+	case LWESP_EVT_CONN_ERROR:{
+		LogDebug(("Callback LWESP_EVT_INIT_FINISH"));
+		break;
+	}
+	case LWESP_EVT_CONN_CLOSE:{
+		LogDebug(("Callback LWESP_EVT_INIT_FINISH"));
+		break;
+	}
+	case LWESP_EVT_CONN_POLL:{
+		LogDebug(("Callback LWESP_EVT_INIT_FINISH"));
+		break;
+	}
+	case LWESP_EVT_SERVER:{
+		LogDebug(("Callback LWESP_EVT_INIT_FINISH"));
+		break;
+	}
+	case LWESP_EVT_WIFI_CONNECTED:{
+		LogDebug(("Callback LWESP_EVT_INIT_FINISH"));
+		break;
+	}
+	case LWESP_EVT_WIFI_GOT_IP:{
+		LogDebug(("Callback LWESP_EVT_INIT_FINISH"));
+		break;
+	}
+	case LWESP_EVT_WIFI_DISCONNECTED:{
+		LogDebug(("Callback LWESP_EVT_INIT_FINISH"));
+		break;
+	}
+	case LWESP_EVT_WIFI_IP_ACQUIRED:{
+		LogDebug(("Callback LWESP_EVT_INIT_FINISH"));
+		break;
+	}
+	case LWESP_EVT_STA_LIST_AP:{
+		LogDebug(("Callback LWESP_EVT_INIT_FINISH"));
+		break;
+	}
+	case LWESP_EVT_STA_JOIN_AP:{
+		LogDebug(("Callback LWESP_EVT_INIT_FINISH"));
+		break;
+	}
+	case LWESP_EVT_STA_INFO_AP:{
+		LogDebug(("Callback LWESP_EVT_INIT_FINISH"));
+		break;
+	}
+	case LWESP_EVT_AP_CONNECTED_STA:{
+		LogDebug(("Callback LWESP_EVT_INIT_FINISH"));
+		break;
+	}
+	case LWESP_EVT_AP_DISCONNECTED_STA:{
+		LogDebug(("Callback LWESP_EVT_INIT_FINISH"));
+		break;
+	}
+	case LWESP_EVT_AP_IP_STA:{
+		LogDebug(("Callback LWESP_EVT_INIT_FINISH"));
+		break;
+	}
+	case LWESP_EVT_DNS_HOSTBYNAME:{
+		LogDebug(("Callback LWESP_EVT_INIT_FINISH"));
+		break;
+	}
+	/*
+	case LWESP_EVT_PING:{
+		LogDebug(("Callback LWESP_EVT_INIT_FINISH"));
+		break;
+	}
+	*/
+	default:{
+		LogDebug(("Callback Other %d", evt->type));
+		break;
+	}
+	}
+
+
+
 	return lwespOK;
 
 }
@@ -95,9 +212,22 @@ void ip2str(char *str, lwesp_ip_t *ip){
 		(int)ip->addr.ip4.addr[2], (int)ip->addr.ip4.addr[3]);
 }
 
+void lwesp_cb(lwespr_t evt, void* pv){
+	printf("Callbackr returned %d\n", evt);
+
+	if (evt == lwespOK){
+		lwesp_ip_t *ip = (lwesp_ip_t *) pv;
+		char ipStr[20];
+		ip2str(ipStr, ip);
+		printf("IP is %s\n", ipStr);
+	}
+
+
+}
+
 
 void
-init_thread(void* pvParameters) {
+init_thread_old(void* pvParameters) {
 	lwespr_t eres;
 	char ipStr[16];
 
@@ -140,12 +270,75 @@ init_thread(void* pvParameters) {
 			 }
         }
         startMQTT();
+
+        lwespr_t smartRes;
+        //smartRes = lwesp_smart_set_config(1, NULL,  NULL, 1);
+        smartRes = lwesp_hostname_set("LS1", NULL, NULL, 1);
+        char host[20];
+        smartRes = lwesp_hostname_get(host, 20, NULL, NULL, 1);
+        switch(smartRes){
+        case lwespOK:{
+        	printf("Hostname is %s\n", host);
+        	break;
+        }
+        case lwespERR: {
+        	printf("Hostname general error\n");
+        	break;
+        }
+        default: {
+        	printf("Hostname failed %d\n", smartRes);
+        }
+        }
+
+        lwesp_ip_t ip;
+        smartRes = lwesp_dns_gethostbyname("pudev.local.jondurrant.com", &ip, lwesp_cb, &ip, 1);
+        switch(smartRes){
+		   case lwespOK:{
+			printf("dns ok \n");
+			ip2str(ipStr, &ip);
+		    printf("IP address: %s\r\n", ipStr);
+			break;
+		   }
+		   case lwespERR: {
+			printf("dns general error\n");
+			break;
+		   }
+		   default: {
+			printf("dns failed %d\n", smartRes);
+		   }
+	    }
     }
+
+
+
     for (;;){
     	vTaskDelay(5000);
 
     }
 }
+
+void
+init_thread(void* pvParameters) {
+	//TwinTask xTwin;
+	if (WifiHelper::connectToAp(SID, PASSWD)){
+		mqttAgent.credentials(mqttUser, mqttPwd);
+		mqttAgent.setObserver(&agentObs);
+		mqttRouter.init(mqttAgent.getId(), &mqttAgent, &state);
+
+		mqttAgent.setRouter(&mqttRouter);
+		mqttAgent.connect(mqttTarget, mqttPort, true);
+
+		mqttAgent.start(tskIDLE_PRIORITY+1);
+		startMQTT();
+	}
+
+    for (;;){
+    	vTaskDelay(5000);
+
+    }
+}
+
+
 
 int main() {
 
@@ -155,16 +348,25 @@ int main() {
 	printf("******GO******\n");
 
 
+	/**
+	WifiAgent wifi(SID, PASSWD);
+	wifi.start();
+	startMQTT();
+	**/
+
+
     BaseType_t atReturned;
 	TaskHandle_t atHandle = NULL;
-	/* Create the task, storing the handle. */
+
 	atReturned = xTaskCreate(
-					init_thread,       /* Function that implements the task. */
-					"Init task",   /* Text name for the task. */
-					512,             /* Stack size in words, not bytes. */
-					( void * ) 1,    /* Parameter passed into the task. */
-					tskIDLE_PRIORITY+1,/* Priority at which the task is created. */
+					init_thread,
+					"Init task",
+					512,
+					( void * ) 1,
+					tskIDLE_PRIORITY+1,
 					&atHandle );
+
+
 
 
     vTaskStartScheduler();
